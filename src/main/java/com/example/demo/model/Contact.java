be0fc963235cc4913bbba9ad0442270b1a362704
Nitya <nityapandey2004@gmail.com>
2026-03-09 12:08:14 +0530

    [Nitya Pandey] : added add contact

--- src/main/java/com/example/demo/model/Contact.java
+++ src/main/java/com/example/demo/model/Contact.java
@@ -1,13 +1,7 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Contact {
+
     private String firstName;
     private String lastName;
     private String address;
@@ -16,4 +10,21 @@ public class Contact {
     private String zip;
     private String phoneNumber;
     private String email;
+
+    public Contact() {}
+
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state, String zip,
+                   String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+
 }
